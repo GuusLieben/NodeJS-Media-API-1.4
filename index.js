310176102c80b0daa4e75646f7144ed48fcc5844
Guus Lieben <guus@xendox.com>
2019-04-17 20:06:45 +0200

    Update index.js, store.js, index.html

--- index.js
+++ index.js
@@ -1,48 +1,82 @@
 const express = require('express');
 const store = require('./store');
+
 const app = express();
 const port = 82;
 
-/* STATIC requests */
-app.use(express.static('static'));
+function user_app() {
+    app.get(store.user_single_suffix, (req, res) => {
+        const random = Math.floor(Math.random() * (store.users.length)) + 1;
+        let id = req.params.userId === 'random' ? random.toString() : req.params.userId;
+        sendJson(req, res, getUserObj(id));
+    });
 
-/* GET requests */
-app.get('/users', (req, res) => res.send(JSON.stringify(store.objectStore.list, null, 2).toString()));
+    app.get(store.user_suffix, (req, res) =>
+        sendJson(req, res, store.users, null, 2));
 
-app.get('/users/:userId', (req, res) => {
-    let id = req.params.userId;
+    app.post(store.user_suffix, (req, res) => {
+        const b = req.query;
+        let user = new store.User(b.name, b.street, b.postcode, b.birthdate, b.phone, b.email, b.password);
+        store.users.push(user);
+        sendJson(req, res, JSON.stringify(user));
+    });
+}
 
-    if (id === 'random') {
-        const random = Math.floor(Math.random() * (store.objectStore.list.length)) + 1;
-        id = random.toString();
-    }
-    res.send(getObject(id));
-});
+function movie_app() {
+    app.get(store.movies_suffix, (req, res) => sendJson(req, res, store.movies, null, 2));
 
-function getObject(id) {
-    const list = store.objectStore.list;
+    app.delete(store.movie_single_suffix, (req, res) => {
+        for (let i = 0; i < store.movies.length; i++) {
+            if (store.movies[i].id == req.params.movieId) {
+                console.log('Removing : ', store.movies[i]);
+                store.movies.splice(i, 1);
+                sendJson(req, res, '{result: true}');
+                return;
+            }
+        }
+        sendJson(req, res, '{result: false};');
+    });
+
+    app.get(store.movie_single_suffix, (req, res) => sendJson(req, res, getMovieObj(req.params.movieId)));
+}
+
+function app_handling() {
+    app.use(express.static('static'));
+
+    app.use((req, res) => {
+        console.log('User attempted to connect to ', req.originalUrl, ' and was sent 404 Not Found');
+        res.status(404).send('This page is as non-existent as my life.');
+    });
+
+    app.use((err, req, res) => {
+        console.log(err.stack);
+        res.status(500).send('This is as broken as my will to live.');
+    });
+}
+
+function getUserObj(id) {
+    const list = store.users;
     for (let i = 0; i < list.length; i++)
-        if (list[i].id === id)
+        if (list[i].id == id)
             return list[i];
-
     return '{}';
 }
 
-/* PUT requests */
-app.put('/user', (req, res) => res.send('Got a PUT request @ /user'));
+function getMovieObj(id) {
+    const list = store.movies;
+    for (let i = 0; i < list.length; i++)
+        if (list[i].id == id)
+            return list[i];
+    return '{}';
+}
 
-/* DELETE requests */
-app.delete('/user', (req, res) => res.send('Got a DELETE request @ /user'));
+function sendJson(req, res, msg) {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Returning to user at ', ip);
+    res.set('Content-Type', 'text/json');
+    res.json(msg);
+}
 
-/* INVALID requests */
-app.use((req, res) => {
-    console.log('User attempted to connect to ', req.originalUrl, ' and was sent 404 Not Found');
-    res.status(404).send('This page is as non-existent as my life.')
-});
-app.use((err, req, res) => {
-    console.log(err.stack);
-    res.status(500).send('This is as broken as my will to live.')
-});
-
-// Start the app
-app.listen(port, () => console.log(`App started on port ${port}!`));
+// Start
+user_app(), movie_app(), app_handling();
+app.listen(port, () => console.log(`App successfully started on ${app.mountpath} on port ${port}!`));
--- store.js
+++ store.js
@@ -1,14 +1,40 @@
-const objectStore = {
-    "list": [
-        {
-            id: '1',
-            name: 'Guus'
-        },
-        {
-            id: '2',
-            name: 'Bob'
-        }
-    ]
-};
+const movies = [];
+movies.push(new Movie('Batman', 'You know what this is', 2015, 'Bob Bobberson'));
+movies.push(new Movie('Niet Batman', 'Don\'t watch this at school', 2019, 'Guus Lieben'));
+module.exports.movies = movies;
 
-module.exports.objectStore = objectStore;
+const users = [];
+users.push(new User('Guus Lieben', 'Arjan-straat 420', '1234 AB', '23/07/2000', '+31 6 2047 3802', 'guus@xendox.com', 'hahahaNee123'));
+users.push(new User('Bob Bobberson', 'Bob-Willem Straat 12', '6969 OI', '01/02/0304', '+12 3456 7890', 'bob@bob.com', 'wieIsBobEigenlijk_'));
+module.exports.users = users;
+
+
+module.exports.base_suffix = '/api';
+
+module.exports.movies_suffix = module.exports.base_suffix + '/movies';
+module.exports.movie_single_suffix = module.exports.movies_suffix + '/:movieId';
+
+module.exports.user_suffix = module.exports.base_suffix + '/register';
+module.exports.user_single_suffix = module.exports.user_suffix + '/:userId';
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
+function Movie(title, descr, release, director) {
+    this.title = title;
+    this.description = descr;
+    this.release = release;
+    this.director = director;
+    this.id = movies.length + 1;
+}
+
+function User(name, street, postcode, birthdate, phone, email, password) {
+    this.name = name;
+    this.street = street;
+    this.postcode = postcode;
+    this.birthdate = birthdate;
+    this.phone = phone;
+    this.email = email;
+    this.password = password;
+    this.id = users.length + 1;
+}
