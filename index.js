ee7e39aaea6fd18a34365167a24c470e4f43fcc0
Guus Lieben <guus@xendox.com>
2019-04-17 21:00:15 +0200

    Add documentation to JS files

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+// Instances of required sources
 const express = require('express');
 const store = require('./store');
-
 const app = express();
+
+// Settings
 const port = 82;
 
+// GET, POST handlers for Users
 function user_app() {
+
+    // GET handler for specific User
     app.get(store.user_single_suffix, (req, res) => {
         const random = Math.floor(Math.random() * (store.users.length)) + 1;
         let id = req.params.userId === 'random' ? random.toString() : req.params.userId;
         sendJson(req, res, getUserObj(id));
     });
 
+    // GET handler for all users
     app.get(store.user_suffix, (req, res) =>
-        sendJson(req, res, store.users, null, 2));
+        sendJson(req, res, store.users));
 
+    // POST handler for new user
     app.post(store.user_suffix, (req, res) => {
         const b = req.query;
         let user = new store.User(b.name, b.street, b.postcode, b.birthdate, b.phone, b.email, b.password);
         store.users.push(user);
-        sendJson(req, res, JSON.stringify(user));
+        sendJson(req, res, user);
     });
 }
 
+// GET, DELETE handlers for Movies
 function movie_app() {
+
+    // GET handler for specific movie
+    app.get(store.movie_single_suffix, (req, res) => sendJson(req, res, getMovieObj(req.params.movieId)));
+
+    // GET handler for all movies
     app.get(store.movies_suffix, (req, res) => sendJson(req, res, store.movies, null, 2));
 
+    // DELETE handler for specific movie
     app.delete(store.movie_single_suffix, (req, res) => {
         for (let i = 0; i < store.movies.length; i++) {
             if (store.movies[i].id == req.params.movieId) {
@@ -36,24 +50,27 @@ function movie_app() {
         }
         sendJson(req, res, '{result: false};');
     });
-
-    app.get(store.movie_single_suffix, (req, res) => sendJson(req, res, getMovieObj(req.params.movieId)));
 }
 
-function app_handling() {
+// Default behavior settings of the app
+function start_app() {
+    // Serve the static page defined in /static, served on /
     app.use(express.static('static'));
 
+    // Handle 404's
     app.use((req, res) => {
-        console.log('User attempted to connect to ', req.originalUrl, ' and was sent 404 Not Found');
+        console.log('User [', req.headers['x-forwarded-for'] || req.connection.remoteAddress, '] attempted to connect to ', req.originalUrl, ' and was sent 404 Not Found');
         res.status(404).send('This page is as non-existent as my life.');
     });
 
+    // Handle 500's
     app.use((err, req, res) => {
         console.log(err.stack);
         res.status(500).send('This is as broken as my will to live.');
     });
 }
 
+// Find User object from user storage, return blank JSON if non-existent
 function getUserObj(id) {
     const list = store.users;
     for (let i = 0; i < list.length; i++)
@@ -62,6 +79,7 @@ function getUserObj(id) {
     return '{}';
 }
 
+// Find Movie object from movie storage, return blank JSON if non-existent
 function getMovieObj(id) {
     const list = store.movies;
     for (let i = 0; i < list.length; i++)
@@ -70,13 +88,14 @@ function getMovieObj(id) {
     return '{}';
 }
 
+// Respond with a JSON element, log client
 function sendJson(req, res, msg) {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('Returning to user at ', ip);
+    console.log('Returning to client at ', ip);
     res.set('Content-Type', 'text/json');
     res.json(msg);
 }
 
-// Start
-user_app(), movie_app(), app_handling();
+// Start the app
+user_app(), movie_app(), start_app();
 app.listen(port, () => console.log(`App successfully started on ${app.mountpath} on port ${port}!`));
--- store.js
+++ store.js
@@ -1,34 +1,25 @@
-const movies = [];
-movies.push(new Movie('Batman', 'You know what this is', 2015, 'Bob Bobberson'));
-movies.push(new Movie('Niet Batman', 'Don\'t watch this at school', 2019, 'Guus Lieben'));
-module.exports.movies = movies;
-
-const users = [];
-users.push(new User('Guus Lieben', 'Arjan-straat 420', '1234 AB', '23/07/2000', '+31 6 2047 3802', 'guus@xendox.com', 'hahahaNee123'));
-users.push(new User('Bob Bobberson', 'Bob-Willem Straat 12', '6969 OI', '01/02/0304', '+12 3456 7890', 'bob@bob.com', 'wieIsBobEigenlijk_'));
-module.exports.users = users;
-
-
+// Default URL Suffixes
 module.exports.base_suffix = '/api';
 
+// URL Suffixes for movies
 module.exports.movies_suffix = module.exports.base_suffix + '/movies';
 module.exports.movie_single_suffix = module.exports.movies_suffix + '/:movieId';
 
+// URL Suffixes for users
 module.exports.user_suffix = module.exports.base_suffix + '/register';
 module.exports.user_single_suffix = module.exports.user_suffix + '/:userId';
 
-module.exports.Movie = Movie;
-module.exports.User = User;
-
-function Movie(title, descr, release, director) {
+// Movie object constructor, exported
+module.exports.Movie = function Movie(title, descr, release, director) {
     this.title = title;
     this.description = descr;
     this.release = release;
     this.director = director;
     this.id = movies.length + 1;
-}
+};
 
-function User(name, street, postcode, birthdate, phone, email, password) {
+// User object constructor, exported
+module.exports.User = function User(name, street, postcode, birthdate, phone, email, password) {
     this.name = name;
     this.street = street;
     this.postcode = postcode;
@@ -37,4 +28,36 @@ function User(name, street, postcode, birthdate, phone, email, password) {
     this.email = email;
     this.password = password;
     this.id = users.length + 1;
-}
+};
+
+// Movie Array
+const movies = [];
+// Populate array with two sample movies
+movies.push(new Movie('Batman', 'You know what this is', 2015, 'Bob Bobberson'));
+movies.push(new Movie('Niet Batman', 'Don\'t watch this at school', 2019, 'Guus Lieben'));
+// Export movie array
+module.exports.movies = movies;
+
+// User Array
+const users = [];
+// Populate array with two sample users
+users.push(
+    new User(
+        'Guus Lieben',
+        'Arjan-straat 420',
+        '1234 AB',
+        '23/07/2000',
+        '+31 6 2047 3802',
+        'guus@xendox.com',
+        'hahahaNee123'));
+users.push(
+    new User(
+        'Bob Bobberson',
+        'Bob-Willem Straat 12',
+        '6969 OI',
+        '01/02/0304',
+        '+12 3456 7890',
+        'bob@bob.com',
+        'wieIsBobEigenlijk_'));
+// Export user array
+module.exports.users = users;
