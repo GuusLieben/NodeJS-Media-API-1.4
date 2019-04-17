1f4cca183ea63dec902c37b8a6d1d1049989dd0c
Guus Lieben <guus@xendox.com>
2019-04-17 22:04:27 +0200

    Add logging, clean up file structure

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // Instances of required sources
 const express = require('express');
-const store = require('./store');
 const app = express();
+const store = require('./sources/store');
+const logger = require('tracer').console({
+    transport: function (data) {
+        console.log(data.output);
+        require('fs').appendFile('./logs/node_api.log', data.rawoutput + '\n', (err) => {
+                if (err) throw err;
+            }
+        )
+        ;
+    }
+});
 
 // Settings
 const port = 82;
@@ -42,7 +52,7 @@ function movie_app() {
     app.delete(store.movie_single_suffix, (req, res) => {
         for (let i = 0; i < store.movies.length; i++) {
             if (store.movies[i].id == req.params.movieId) { // Intentional possibility of type coercion (number <> string)
-                console.log('Removing : ', store.movies[i]);
+                logger.warn('Removing : ', store.movies[i]);
                 store.movies.splice(i, 1);
                 sendJson(req, res, '{result: true}');
                 return;
@@ -57,16 +67,16 @@ function start_app() {
     // Serve the static page defined in /static, served on /
     app.use(express.static('static'));
 
-    // Handle 404's
+    // Handle 404's -> Not found
     app.use((req, res) => {
-        console.log('User [', req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        logger.debug('User [', req.headers['x-forwarded-for'] || req.connection.remoteAddress,
             '] attempted to connect to ', req.originalUrl, ' and was sent 404 Not Found');
         res.status(404).send('This page is as non-existent as my life.');
     });
 
-    // Handle 500's
+    // Handle 500's -> Internal error
     app.use((err, req, res) => {
-        console.log(err.stack);
+        logger.error(err.stack);
         res.status(500).send('This is as broken as my will to live.');
     });
 }
@@ -74,31 +84,31 @@ function start_app() {
 // Get User object from stored users
 function getUserObj(id) {
     const arr = store.users;
-    console.log('Searching for user ..');
+    logger.debug('Searching for user ..');
     return getObjectFromArray(id, arr);
 }
 
 // Get Movie object from stored movies
 function getMovieObj(id) {
     const arr = store.movies;
-    console.log('Searching for movie ..');
+    logger.debug('Searching for movie ..');
     return getObjectFromArray(id, arr);
 }
 
 // Find object from given array, returns blank array if non-existent
 function getObjectFromArray(id, arr) {
-    console.log('Searching for object with id ', id, ' in a list of ', arr.length, ' objects');
+    logger.debug('Searching for object with id ', id, ' in a list of ', arr.length, ' objects');
     return arr.filter(obj => obj.id == id); // Intentional possibility of type coercion (number <> string)
 }
 
 // Respond with a JSON element, log client
 function sendJson(req, res, msg) {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('Returning to client at ', ip);
+    logger.log('Returning to client at ', ip);
     res.set('Content-Type', 'text/json');
     res.json(msg);
 }
 
 // Start the app
 user_app(), movie_app(), start_app();
-app.listen(port, () => console.log(`App successfully started on ${app.mountpath} on port ${port}!`));
+app.listen(port, () => logger.log(`App successfully started on ${app.mountpath} on port ${port}!`));
